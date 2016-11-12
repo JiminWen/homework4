07bd1eac5883db099bddd7b95e72cc83e6fd1a48
Jimin Wen <jiminwen@tamu.edu>
2016-11-12 22:18:02 +0000

    pass one scenatio

--- rottenpotatoes/features/step_definitions/my_steps.rb
+++ rottenpotatoes/features/step_definitions/my_steps.rb
@@ -9,3 +9,15 @@ end
 When (/^(?:|I) go to the (.+)$/) do |page_name|
     visit path_to(page_name)
 end
+
+When /^(?:|I )fill in "([^"]*)" with "([^"]*)"$/ do |field, value|
+  fill_in(field, :with => value)
+end
+
+When /^(?:|I) press "([^"]*)"$/ do |button|
+  click_button(button) 
+end
+
+Then /^(?:|the) director of "([^"]*)" should be "([^"]*)"$/ do |first,second|
+    page.body.should=~/#{first}.*Director.*#{second}/m
+end
